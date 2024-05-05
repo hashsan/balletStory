3c2b085fa14e1bb9c1456c8f322fafe075042946
はっしゅさん <149641384+hashsan@users.noreply.github.com>
2024-05-05 18:05:23 +0900

    Add files via upload

--- functions.php
+++ functions.php
@@ -1,6 +1,12 @@
 <?php
+//style.css読み込み
+function your_theme_enqueue_styles() {
+    wp_enqueue_style( 'your-theme-style', get_stylesheet_uri() );
+}
+add_action( 'wp_enqueue_scripts', 'your_theme_enqueue_styles' );
+
 // クラシックエディタを使用するように指示する
-add_filter( 'use_block_editor_for_post', '__return_false' );
+//add_filter( 'use_block_editor_for_post', '__return_false' );
 
 // ウィジェットエリアを追加する
 function your_theme_widgets_init() {
@@ -27,3 +33,37 @@ function your_theme_widgets_init() {
     ) );
 }
 add_action( 'widgets_init', 'your_theme_widgets_init' );
+
+
+//構造化データ追加 excerpt()の指示が必要
+function add_structured_data_to_head() {
+    if ( is_single() ) {
+        global $post;
+        setup_postdata( $post );
+        
+        $data = array(
+            '@context' => 'http://schema.org',
+            '@type' => 'BlogPosting',
+            'headline' => get_the_title(),
+            'datePublished' => get_the_date( 'c' ),
+            'dateModified' => get_the_modified_date( 'c' ),
+            'description' => get_the_excerpt(),
+            'author' => array(
+                '@type' => 'Person',
+                'name' => get_the_author_meta( 'display_name' ),
+            ),
+            'keywords' => wp_get_post_tags( get_the_ID(), array( 'fields' => 'names' ) ), // タグを追加
+            'articleSection' => wp_get_post_categories( get_the_ID(), array( 'fields' => 'names' ) ), // カテゴリを追加
+        );
+
+        echo '<script type="application/ld+json">' . wp_json_encode( $data ) . '</script>';
+        wp_reset_postdata();
+    }
+}
+add_action( 'wp_head', 'add_structured_data_to_head' );
+
+
+
+
+
+
--- index.php
+++ index.php
@@ -22,9 +22,9 @@
         <div class="main-content">
             <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
                 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                    <header class="entry-header">
-                        <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                    </header>
+                    <div class="entry-header">
+                        <h1 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+                    </div>
                     <div class="entry-content">
                         <?php the_content(); ?>
                     </div>
